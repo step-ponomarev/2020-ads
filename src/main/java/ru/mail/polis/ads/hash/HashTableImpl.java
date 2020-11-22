20e315b0d7daabb3e916eef43b3a8ef25f41c6bc
stepan.ponomarev <stepan.ponomarev@WH0102950.local>
2020-11-22 17:41:21 +0300

    started

---
+++ src/main/java/ru/mail/polis/ads/hash/HashTableImpl.java
@@ -0,0 +1,119 @@
+package ru.mail.polis.ads.hash;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class HashTableImpl implements HashTable {
+    private int arraySize;
+    private int elementAmount;
+    private final List<LinkedList<Node>> buckets;
+
+    private static class Node {
+        private Object key;
+        private Object value;
+
+        public Node(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Object getKey() {
+            return key;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return key.equals(node.key) &&
+                    value.equals(node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key, value);
+        }
+    }
+
+    public HashTableImpl() {
+        elementAmount = 0;
+        arraySize = 16;
+        this.buckets = new ArrayList<LinkedList<Node>>(arraySize);
+    }
+
+    @Nullable
+    @Override
+    public Object get(@NotNull Object o) {
+        Node node = getNodeByKey(o);
+
+        return node == null ? null : node.getValue();
+    }
+
+    @Override
+    public void put(@NotNull Object o, @NotNull Object o2) {
+        List<Node> bucket = getBucketByHash(o.hashCode());
+
+        bucket.add(new Node(o, o2));
+        // TODO: Переложить бакеты
+
+        elementAmount++;
+    }
+
+    @Nullable
+    @Override
+    public Object remove(@NotNull Object o) {
+        List<Node> bucket = getBucketByHash(o.hashCode());
+
+        Node getNode = getNodeByKey(o);
+
+        bucket.remove(getNode);
+
+        // TODO: Переложить бакеты
+
+        elementAmount--;
+
+        return getNode;
+    }
+
+    @Override
+    public int size() {
+        return elementAmount;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return elementAmount == 0;
+    }
+
+    private Node getNodeByKey(@NotNull Object o) {
+        List<Node> bucket = getBucketByHash(o.hashCode());
+
+        for (Node node : bucket) {
+            if (node.getKey().equals(o)) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    private List<Node> getBucketByHash(int hash) {
+        int i = hash % arraySize;
+
+        return buckets.get(i);
+    }
+
+    private float getLoadState() {
+        return arraySize / elementAmount;
+    }
+}
